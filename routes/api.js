dae9f334c9b9f0e24a6b93114a5dfd9d4640e743
williammb <wmb96@hotmail.com>
2019-08-25 10:28:12 -0300

    complete task

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,155 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
 'use strict';
 
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
-
+const request = require('request');
+require('dotenv').config();
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app) {
+module.exports = function(app) {
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+    app.route('/api/stock-prices')
+        .get(function(req, res) {
+            if (!req.query.stock) {
+                res.json({ error: 'no stock provide' });
+            } else if (!Array.isArray(req.query.stock)) {
+                MongoClient.connect(CONNECTION_STRING, (err, db) => {
+                    if (err) {
+                        res.json({ error: 'Database error: ' + err });
+                    }
+                    if (!req.query.like) {
+                        db.collection('stocklikes').find({ stock: req.query.stock }).toArray((err, likes) => {
+                            if (err) {
+                                res.json({ error: 'Error find: ' + err });
+                            }
+                            const url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + req.query.stock + '&apikey=' + process.env.APIKEY;
+                            request(url, function(error, response, body) {
+                                if (!error && response.statusCode == 200) {
+                                    if (!JSON.parse(body)['Error Message']) {
+                                        const getStock = JSON.parse(body)['Global Quote'];
+                                        res.json({ "stockData": { "stock": getStock['01. symbol'], "price": getStock['05. price'], "likes": likes.length } })
+                                    } else {
+                                        res.json({ error: 'Stock dont exist' });
+                                    }
+                                }
+                            });
+                        })
+                    } else {
+                        const stocklike = {
+                            stock: req.query.stock,
+                            ip: req.connection.remoteAddress
+                        }
+                        db.collection('stocklikes').findOne(stocklike, (err, like) => {
+                            if (err) {
+                                res.json({ error: 'Error find: ' + err });
+                            }
+                            if (!like) {
+                                db.collection('stocklikes').insertOne(stocklike, (err, data) => {
+                                    if (err) {
+                                        res.json({ error: 'Erro to save this book' });
+                                    }
+                                    db.collection('stocklikes').find({ stock: req.query.stock }).toArray((err, likes) => {
+                                        if (err) {
+                                            res.json({ error: 'Error find: ' + err });
+                                        }
+                                        const url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + req.query.stock + '&apikey=' + process.env.APIKEY;
+                                        request(url, function(error, response, body) {
+                                            if (!error && response.statusCode == 200) {
+                                                if (!JSON.parse(body)['Error Message']) {
+                                                    const getStock = JSON.parse(body)['Global Quote'];
+                                                    res.json({ "stockData": { "stock": getStock['01. symbol'], "price": getStock['05. price'], "likes": likes.length } })
+                                                } else {
+                                                    res.json({ error: 'Stock dont exist' });
+                                                }
+                                            }
+                                        });
+                                    })
+                                })
+                            } else {
+                                db.collection('stocklikes').find({ stock: req.query.stock }).toArray((err, likes) => {
+                                    if (err) {
+                                        res.json({ error: 'Error find: ' + err });
+                                    }
+                                    const url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + req.query.stock + '&apikey=' + process.env.APIKEY;
+                                    request(url, function(error, response, body) {
+                                        if (!error && response.statusCode == 200) {
+                                            if (!JSON.parse(body)['Error Message']) {
+                                                const getStock = JSON.parse(body)['Global Quote'];
+                                                //console.log(JSON.parse(body)['Global Quote'])
+                                                res.json({ "stockData": { "stock": getStock['01. symbol'], "price": getStock['05. price'], "likes": likes.length } })
+                                            } else {
+                                                res.json({ error: 'Stock dont exist' });
+                                            }
+                                        }
+                                    });
+                                })
+                            }
+                        })
+
+                    }
+                })
+            } else {
+                if (!req.query.like) {
+                    const url = 'http://localhost:3000/api/stock-prices?stock='
+                    request(url + req.query.stock[0], function(error, firstresponse, firstbody) {
+                        if (!error && firstresponse.statusCode == 200) {
+                            if (!JSON.parse(firstbody)[error]) {
+                                const firstgetStock = JSON.parse(firstbody)['stockData'];
+                                request(url + req.query.stock[1], function(error, response, body) {
+                                    if (!error && response.statusCode == 200) {
+                                        if (!JSON.parse(body)[error]) {
+                                            const secondgetStock = JSON.parse(body)['stockData'];
+                                            const relative = firstgetStock.likes - secondgetStock.likes;
+                                            res.json({
+                                                "stockData": [{ "stock": firstgetStock.stock, "price": firstgetStock.price, "rel_likes": firstgetStock.likes - relative },
+                                                    { "stock": secondgetStock.stock, "price": secondgetStock.price, "rel_likes": secondgetStock.likes - relative }
+                                                ]
+                                            })
+                                        } else {
+                                            res.json(JSON.parse(body)[error])
+                                        }
+                                    }
+                                })
+                            } else {
+                                res.json(JSON.parse(body)[error])
+                            }
+                        }
+                    })
+                } else {
+                    const url = 'http://localhost:3000/api/stock-prices?stock='
+                    request(url + req.query.stock[0] + '&like=true', function(error, firstresponse, firstbody) {
+                        if (!error && firstresponse.statusCode == 200) {
+                            if (!JSON.parse(firstbody)[error]) {
+                                const firstgetStock = JSON.parse(firstbody)['stockData'];
+                                request(url + req.query.stock[1] + '&like=true', function(error, response, body) {
+                                    if (!error && response.statusCode == 200) {
+                                        if (!JSON.parse(body)[error]) {
+                                            const secondgetStock = JSON.parse(body)['stockData'];
+                                            const relative = firstgetStock.likes - secondgetStock.likes;
+                                            res.json({
+                                                "stockData": [{ "stock": firstgetStock.stock, "price": firstgetStock.price, "rel_likes": firstgetStock.likes - relative },
+                                                    { "stock": secondgetStock.stock, "price": secondgetStock.price, "rel_likes": secondgetStock.likes - relative }
+                                                ]
+                                            })
+                                        } else {
+                                            res.json(JSON.parse(body)[error])
+                                        }
+                                    }
+                                })
+                            } else {
+                                res.json(JSON.parse(body)[error])
+                            }
+                        }
+                    })
+                }
+            }
+        });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,10 @@
 /*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
+ *
+ *
+ *       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+ *       -----[Keep the tests in the same order!]-----
+ *       (if additional are added, keep them at the very end!)
+ */
 
 var chaiHttp = require('chai-http');
 var chai = require('chai');
@@ -14,37 +14,97 @@ var server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-    
+
     suite('GET /api/stock-prices => stockData object', function() {
-      
-      test('1 stock', function(done) {
-       chai.request(server)
-        .get('/api/stock-prices')
-        .query({stock: 'goog'})
-        .end(function(err, res){
-          
-          //complete this one too
-          
-          done();
+
+        test('1 stock', function(done) {
+            this.timeout(10000);
+            chai.request(server)
+                .get('/api/stock-prices')
+                .query({ stock: 'goog' })
+                .end(function(err, res) {
+                    assert.equal(res.status, 200);
+                    assert.isString(res.body.stockData.stock);
+                    assert.isString(res.body.stockData.price);
+                    assert.isNumber(res.body.stockData.likes);
+                    assert.equal(res.body.stockData.stock, 'GOOG');
+                    assert.property(res.body.stockData, 'stock')
+                    assert.property(res.body.stockData, 'price')
+                    assert.property(res.body.stockData, 'likes')
+                    done();
+                });
         });
-      });
-      
-      test('1 stock with like', function(done) {
-        
-      });
-      
-      test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
-      });
-      
-      test('2 stocks', function(done) {
-        
-      });
-      
-      test('2 stocks with like', function(done) {
-        
-      });
-      
+
+        test('1 stock with like', function(done) {
+            this.timeout(10000);
+            chai.request(server)
+                .get('/api/stock-prices')
+                .query({ stock: 'goog', like: true })
+                .end(function(err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.stockData.stock, 'GOOG');
+                    assert.equal(res.body.stockData.likes, 2);
+                    assert.property(res.body.stockData, 'stock')
+                    assert.property(res.body.stockData, 'price')
+                    assert.property(res.body.stockData, 'likes')
+                    done();
+                });
+        });
+
+        test('1 stock with like again (ensure likes arent double counted)', function(done) {
+            this.timeout(10000);
+            chai.request(server)
+                .get('/api/stock-prices')
+                .query({ stock: 'goog', like: true })
+                .end(function(err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.stockData.stock, 'GOOG');
+                    assert.equal(res.body.stockData.likes, 2);
+                    assert.property(res.body.stockData, 'stock')
+                    assert.property(res.body.stockData, 'price')
+                    assert.property(res.body.stockData, 'likes')
+                    done();
+                });
+        });
+
+        test('2 stocks', function(done) {
+            this.timeout(10000);
+            chai.request(server)
+                .get('/api/stock-prices')
+                .query({ stock: ['goog', 'MSFT'] })
+                .end(function(err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.stockData[0].stock, 'GOOG');
+                    assert.equal(res.body.stockData[1].stock, 'MSFT');
+                    assert.property(res.body.stockData[0], 'stock')
+                    assert.property(res.body.stockData[0], 'price')
+                    assert.property(res.body.stockData[0], 'rel_likes')
+                    assert.property(res.body.stockData[1], 'stock')
+                    assert.property(res.body.stockData[1], 'price')
+                    assert.property(res.body.stockData[1], 'rel_likes')
+                    done();
+                });
+        });
+
+        test('2 stocks with like', function(done) {
+            this.timeout(10000);
+            chai.request(server)
+                .get('/api/stock-prices')
+                .query({ stock: ['goog', 'MSFT'], like: true })
+                .end(function(err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.stockData[0].stock, 'GOOG');
+                    assert.equal(res.body.stockData[1].stock, 'MSFT');
+                    assert.property(res.body.stockData[0], 'stock')
+                    assert.property(res.body.stockData[0], 'price')
+                    assert.property(res.body.stockData[0], 'rel_likes')
+                    assert.property(res.body.stockData[1], 'stock')
+                    assert.property(res.body.stockData[1], 'price')
+                    assert.property(res.body.stockData[1], 'rel_likes')
+                    done();
+                });
+        });
+
     });
 
 });
